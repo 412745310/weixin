d2661b44dbef4e3f4d02d8778680182cdb8b196f
412745310@qq.com <412745310@qq.com>
2017-06-11 10:02:41 +0800

    更新job单元测试

--- src/main/java/com/chelsea/weixin/job/impl/TokenJob.java
+++ src/main/java/com/chelsea/weixin/job/impl/TokenJob.java
@@ -1,25 +1,50 @@
 package com.chelsea.weixin.job.impl;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import redis.clients.jedis.JedisCluster;
+
+import com.chelsea.weixin.domain.Token;
 import com.chelsea.weixin.job.BaseJob;
+import com.chelsea.weixin.util.TokenUtil;
 
 /**
  * 凭证job
+ * 
  * @author baojun
  *
  */
-public class TokenJob extends BaseJob{
-	
+@Component
+public class TokenJob extends BaseJob {
+
 	Logger logger = LoggerFactory.getLogger(TokenJob.class);
-	
+
+	private final static String ACCESS_TOKEN_KEY = "access_token";
+
+	@Autowired
+	private TokenUtil tokenUtil;
+
+	@Resource(name = "jedisCluster")
+	JedisCluster jedisCluster;
+
 	/**
 	 * 获取access_token并更新到redis
 	 */
 	@Override
 	public void process() {
-		logger.info("do TokenJob process");
+		logger.info("开始执行TokenJob...");
+		Token token = tokenUtil.getToken();
+		String accessToken = token.getAccessToken();
+		int expiresIn = token.getExpiresIn();
+		logger.info("获取到accessToken：" + accessToken);
+		logger.info("获取到expiresIn：" + expiresIn);
+		jedisCluster.del(ACCESS_TOKEN_KEY);
+		jedisCluster.set(ACCESS_TOKEN_KEY, accessToken, "NX", "EX", expiresIn);
 	}
-	
+
 }
