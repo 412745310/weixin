3c3b485290da1c14bc2804c978934b784e46d759
412745310@qq.com <412745310@qq.com>
2017-06-13 23:51:39 +0800

    新增实体bean

---
+++ src/main/java/com/chelsea/weixin/domain/menu/Button.java
@@ -0,0 +1,21 @@
+package com.chelsea.weixin.domain.menu;
+
+/**
+ * 菜单项的基类
+ * 
+ * @author shevchenko
+ *
+ */
+public class Button {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/main/java/com/chelsea/weixin/domain/menu/Menu.java
@@ -0,0 +1,21 @@
+package com.chelsea.weixin.domain.menu;
+
+/**
+ * 整个菜单对象的封装
+ * 
+ * @author shevchenko
+ *
+ */
+public class Menu {
+
+	private Button[] button;
+
+	public Button[] getButton() {
+		return button;
+	}
+
+	public void setButton(Button[] button) {
+		this.button = button;
+	}
+
+}
