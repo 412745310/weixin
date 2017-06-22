d0d204a862b69e55a1582b6b8c2320c1d7a8c287
412745310@qq.com <412745310@qq.com>
2017-06-22 23:49:32 +0800

    修改方法-授权认证

--- src/main/java/com/chelsea/weixin/controller/WeixinController.java
+++ src/main/java/com/chelsea/weixin/controller/WeixinController.java
@@ -6,17 +6,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
+import com.chelsea.weixin.domain.SNSUserInfo;
 import com.chelsea.weixin.domain.WeixinUserInfo;
 import com.chelsea.weixin.domain.menu.Button;
 import com.chelsea.weixin.domain.menu.CommonButton;
 import com.chelsea.weixin.domain.menu.ComplexButton;
 import com.chelsea.weixin.domain.menu.Menu;
 import com.chelsea.weixin.service.WeixinService;
+import com.chelsea.weixin.util.CommonUtil;
+import com.chelsea.weixin.util.Constant;
 import com.chelsea.weixin.util.SignUtil;
 
 /**
@@ -25,15 +30,24 @@ import com.chelsea.weixin.util.SignUtil;
  * @author shevchenko
  *
  */
-@RestController
+@Controller
 @RequestMapping("/weixin")
-public class WeixinController {
+public class WeixinController extends SpringBootServletInitializer{
 
 	private Logger logger = LoggerFactory.getLogger(WeixinController.class);
 
 	// 与接口配置信息中的Token要一致
 	@Value("${weixin.token}")
 	private String token;
+	
+	@Value("${weixin.appid}")
+	private String appid;
+	
+	@Value("${weixin.oauth2AuthorizeUrl}")
+	private String oauth2AuthorizeUrl;
+	
+	@Value("${weixin.oauth2RedirectUrl}")
+	private String oauth2RedirectUrl;
 
 	@Autowired
 	private WeixinService weixinService;
@@ -81,69 +95,29 @@ public class WeixinController {
 	@RequestMapping("/create_menu")
 	public void createMenu() {
 		CommonButton btn11 = new CommonButton();
-		btn11.setName("天气预报");
-		btn11.setType("click");
-		btn11.setKey("11");
-
+		btn11.setName("百度");
+		btn11.setType("view");
+		btn11.setUrl("http://www.baidu.com");
+		
 		CommonButton btn12 = new CommonButton();
-		btn12.setName("公交查询");
-		btn12.setType("click");
-		btn12.setKey("12");
-
-		CommonButton btn13 = new CommonButton();
-		btn13.setName("周边搜索");
-		btn13.setType("click");
-		btn13.setKey("13");
-
-		CommonButton btn14 = new CommonButton();
-		btn14.setName("历史上的今天");
-		btn14.setType("click");
-		btn14.setKey("14");
-
+		btn12.setName("用户信息");
+		btn12.setType("view");
+		String url = oauth2AuthorizeUrl.replace("APPID", appid);
+		url = url.replace("STATE", "123");
+		url = url.replace("REDIRECT_URI", CommonUtil.urlEncodeUTF8(oauth2RedirectUrl));
+		url = url.replace("SCOPE", Constant.SNSAPI_USERINFO);
+		btn12.setUrl(url);
+		logger.info("-----> url :" + url);
+		
 		CommonButton btn21 = new CommonButton();
 		btn21.setName("歌曲点播");
 		btn21.setType("click");
 		btn21.setKey("21");
 
-		CommonButton btn22 = new CommonButton();
-		btn22.setName("经典游戏");
-		btn22.setType("click");
-		btn22.setKey("22");
-
-		CommonButton btn23 = new CommonButton();
-		btn23.setName("美女电台");
-		btn23.setType("click");
-		btn23.setKey("23");
-
-		CommonButton btn24 = new CommonButton();
-		btn24.setName("人脸识别");
-		btn24.setType("click");
-		btn24.setKey("24");
-
-		CommonButton btn25 = new CommonButton();
-		btn25.setName("聊天唠嗑");
-		btn25.setType("click");
-		btn25.setKey("25");
-
 		CommonButton btn31 = new CommonButton();
 		btn31.setName("Q友圈");
 		btn31.setType("click");
 		btn31.setKey("31");
-
-		CommonButton btn32 = new CommonButton();
-		btn32.setName("电影排行榜");
-		btn32.setType("click");
-		btn32.setKey("32");
-
-		CommonButton btn33 = new CommonButton();
-		btn33.setName("幽默笑话");
-		btn33.setType("click");
-		btn33.setKey("33");
-		
-		CommonButton btn34 = new CommonButton();
-		btn34.setName("百度");
-		btn34.setType("view");
-		btn34.setUrl("http://www.baidu.com");
 
 		/**
 		 * 微信： mainBtn1,mainBtn2,mainBtn3底部的三个一级菜单。
@@ -152,16 +126,15 @@ public class WeixinController {
 		ComplexButton mainBtn1 = new ComplexButton();
 		mainBtn1.setName("生活助手");
 		// 一级下有4个子菜单
-		mainBtn1.setSub_button(new CommonButton[] { btn11, btn12, btn13, btn14 });
+		mainBtn1.setSub_button(new CommonButton[] { btn11, btn12});
 
 		ComplexButton mainBtn2 = new ComplexButton();
 		mainBtn2.setName("休闲驿站");
-		mainBtn2.setSub_button(new CommonButton[] { btn21, btn22, btn23, btn24,
-				btn25 });
+		mainBtn2.setSub_button(new CommonButton[] { btn21});
 
 		ComplexButton mainBtn3 = new ComplexButton();
 		mainBtn3.setName("更多体验");
-		mainBtn3.setSub_button(new CommonButton[] { btn31, btn32, btn33, btn34 });
+		mainBtn3.setSub_button(new CommonButton[] { btn31});
 
 		/**
 		 * 封装整个菜单
@@ -180,5 +153,25 @@ public class WeixinController {
 	public WeixinUserInfo getUserInfo(String openid){
 		return weixinService.getUserInfo(openid);
 	}
-
+	
+	/**
+	 * 授权认证
+	 */
+	@RequestMapping("/authorize")
+	public String authorize(ModelMap map, String code, String state){
+		SNSUserInfo snsUserInfo = new SNSUserInfo();
+		if(!"authdeny".equals(code)){
+			snsUserInfo = weixinService.authorize(code);
+		}
+		map.put("snsUserInfo", snsUserInfo);
+		map.put("state", 200);
+	    return "index";
+	}
+	
+	public static void main(String[] args) {
+		String oauth2AuthorizeUrl = "123123|dasdsad";
+		String[] url = oauth2AuthorizeUrl.split("\\|");
+		System.out.println(url[0]);
+	}
+	
 }
