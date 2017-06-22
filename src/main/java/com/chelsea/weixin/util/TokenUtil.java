3ffcd65f406a7acd9fb031581add5f14fa94ce7b
412745310@qq.com <412745310@qq.com>
2017-06-22 23:52:38 +0800

    新增方法-获得网页授权token

--- src/main/java/com/chelsea/weixin/util/TokenUtil.java
+++ src/main/java/com/chelsea/weixin/util/TokenUtil.java
@@ -1,14 +1,15 @@
 package com.chelsea.weixin.util;
 
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import net.sf.json.JSONException;
-import net.sf.json.JSONObject;
-
 import com.chelsea.weixin.domain.Token;
+import com.chelsea.weixin.domain.WeixinOauth2Token;
 
 /**
  * 凭证工具类
@@ -27,8 +28,11 @@ public class TokenUtil {
 
 	@Value("${weixin.tokenurl}")
 	private String tokenurl;
+	
+	@Value("${weixin.oauth2AccessTokenUrl}")
+	private String oauth2AccessTokenUrl;
 
-	private static Logger log = LoggerFactory.getLogger(TokenUtil.class);
+	private static Logger logger = LoggerFactory.getLogger(TokenUtil.class);
 
 	/**
 	 * 获取接口访问凭证
@@ -54,12 +58,42 @@ public class TokenUtil {
 			} catch (JSONException e) {
 				token = null;
 				// 获取token失败
-				log.error("获取token失败 errcode:{} errmsg:{}",
+				logger.error("获取token失败 errcode:{} errmsg:{}",
 						jsonObject.getInt("errcode"),
 						jsonObject.getString("errmsg"));
 			}
 		}
 		return token;
 	}
+	
+	/**
+	 * 获得网页授权token
+	 * @param code
+	 * @return
+	 */
+	public WeixinOauth2Token getOauth2AccessToken(String code) {
+        WeixinOauth2Token wat = null;
+        // 拼接请求地址
+        String requestUrl = oauth2AccessTokenUrl.replace("APPID", appid).replace("SECRET", appsecret).replace("CODE", code);
+        // 获取网页授权凭证
+        JSONObject jsonObject = HttpsUtil.httpsRequest(requestUrl, "GET", null);
+        if (null != jsonObject) {
+            try {
+                wat = new WeixinOauth2Token();
+                wat.setAccessToken(jsonObject.getString("access_token"));
+                wat.setExpiresIn(jsonObject.getInt("expires_in"));
+                wat.setRefreshToken(jsonObject.getString("refresh_token"));
+                wat.setOpenId(jsonObject.getString("openid"));
+                wat.setScope(jsonObject.getString("scope"));
+            } catch (Exception e) {
+                wat = null;
+                int errorCode = jsonObject.getInt("errcode");
+                String errorMsg = jsonObject.getString("errmsg");
+                logger.error("获取网页授权凭证失败 errcode:{} errmsg:{}", errorCode, errorMsg);
+            }
+        }
+        return wat;
+    }
+
 
 }
