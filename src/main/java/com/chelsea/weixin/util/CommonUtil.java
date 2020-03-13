b46fa11a2f288143ad350f072a1e1afccb9af868
baojun_a <baojun_a@aspirecn.com>
2020-03-13 18:59:56 +0800

    新增方法远程图片保存到本地

--- src/main/java/com/chelsea/weixin/util/CommonUtil.java
+++ src/main/java/com/chelsea/weixin/util/CommonUtil.java
@@ -1,21 +1,41 @@
 package com.chelsea.weixin.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Formatter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 /**
  * 通用工具类
+ * 
  * @author shevchenko
  *
  */
+@Component
 public class CommonUtil {
-
-	/**
-	 * URL编码（utf-8）
-	 * @param source
-	 * @return
-	 */
-	public static String urlEncodeUTF8(String source) {
+    
+    private static String downloadPicTempUrl;
+    
+    @Value("${downloadPicTempUrl}")
+    public void setDownloadPicTempUrl(String downloadPicTempUrl_t) {
+        downloadPicTempUrl = downloadPicTempUrl_t;
+    }
+    
+    /**
+     * URL编码（utf-8）
+     * 
+     * @param source
+     * @return
+     */
+    public static String urlEncodeUTF8(String source) {
         String result = source;
         try {
             result = java.net.URLEncoder.encode(source, "utf-8");
@@ -24,13 +44,14 @@ public class CommonUtil {
         }
         return result;
     }
-	
-	/**
-	 * 字符串加密辅助方法
-	 * @param hash
-	 * @return
-	 */
-	public static String byteToHex(final byte[] hash) {
+
+    /**
+     * 字符串加密辅助方法
+     * 
+     * @param hash
+     * @return
+     */
+    public static String byteToHex(final byte[] hash) {
         Formatter formatter = new Formatter();
         for (byte b : hash) {
             formatter.format("%02x", b);
@@ -40,36 +61,66 @@ public class CommonUtil {
         return result;
     }
 
-	/**
-	 * 方法名：byteToStr</br> 详述：将字节数组转换为十六进制字符串</br>
-	 * 
-	 * @param byteArray
-	 * @return
-	 * @throws
-	 */
-	public static String byteToStr(byte[] byteArray) {
-		String strDigest = "";
-		for (int i = 0; i < byteArray.length; i++) {
-			strDigest += byteToHexStr(byteArray[i]);
-		}
-		return strDigest;
-	}
+    /**
+     * 方法名：byteToStr</br> 详述：将字节数组转换为十六进制字符串</br>
+     * 
+     * @param byteArray
+     * @return
+     * @throws
+     */
+    public static String byteToStr(byte[] byteArray) {
+        String strDigest = "";
+        for (int i = 0; i < byteArray.length; i++) {
+            strDigest += byteToHexStr(byteArray[i]);
+        }
+        return strDigest;
+    }
 
-	/**
-	 * 方法名：byteToHexStr</br> 详述：将字节转换为十六进制字符串</br>
-	 * 
-	 * @param mByte
-	 * @return
-	 * @throws
-	 */
-	private static String byteToHexStr(byte mByte) {
-		char[] Digit = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A',
-				'B', 'C', 'D', 'E', 'F' };
-		char[] tempArr = new char[2];
-		tempArr[0] = Digit[(mByte >>> 4) & 0X0F];
-		tempArr[1] = Digit[mByte & 0X0F];
-		String s = new String(tempArr);
-		return s;
-	}
-	
+    /**
+     * 方法名：byteToHexStr</br> 详述：将字节转换为十六进制字符串</br>
+     * 
+     * @param mByte
+     * @return
+     * @throws
+     */
+    private static String byteToHexStr(byte mByte) {
+        char[] Digit = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        char[] tempArr = new char[2];
+        tempArr[0] = Digit[(mByte >>> 4) & 0X0F];
+        tempArr[1] = Digit[mByte & 0X0F];
+        String s = new String(tempArr);
+        return s;
+    }
+
+    /**
+     * 远程图片保存到本地
+     * 
+     * @param picUrl
+     */
+    public static String downloadPicture(String picUrl) {
+        URL url = null;
+        try {
+            url = new URL(picUrl);
+            DataInputStream dataInputStream = new DataInputStream(url.openStream());
+
+            FileOutputStream fileOutputStream = new FileOutputStream(new File(downloadPicTempUrl));
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while ((length = dataInputStream.read(buffer)) > 0) {
+                output.write(buffer, 0, length);
+            }
+            fileOutputStream.write(output.toByteArray());
+            dataInputStream.close();
+            fileOutputStream.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return downloadPicTempUrl;
+    }
+
 }
